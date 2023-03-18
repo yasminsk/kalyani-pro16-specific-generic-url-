0bb5ab41ebbb096b92255e20f8bff618bcb8550a
kalyani <gandikotakalyani334@gmail.com>
2023-03-19 00:16:51 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def Shalini(request):
+    return render(request,'Shalini.html')
---
+++ app2/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def Suseela(request):
+    return render(request,'Suseela.html')
